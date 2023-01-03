2825a9a7bc09fbebf4e2b287c2709951b1eed770
RickySin11 <rajvir11singh@outlook.com>
2023-01-02 22:04:08 -0800

    Configure App settings, Implement Database Manager to Create Database Table

---
+++ SinghxRaj.CodingTracker/SinghxRaj.CodingTracker/Program.cs
@@ -0,0 +1,11 @@
+﻿using SinghxRaj.CodingTracker;
+using System.Configuration;
+
+internal class Program
+{
+    static string connectionString = ConfigurationManager.AppSettings.Get("connectionString")!;
+    private static void Main(string[] args)
+    {
+        DatabaseManager.CreateTable(connectionString);
+    }
+}
