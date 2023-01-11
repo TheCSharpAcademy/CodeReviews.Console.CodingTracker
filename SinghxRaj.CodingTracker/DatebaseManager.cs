7c03c2a4b4571028be1379e6596b350cb78bb8cd
RickySin11 <69612398+SinghxRaj@users.noreply.github.com>
2023-01-11 15:57:56 -0800

    Add Implementation for CreateTable, AddNewCodingSession, and GetCodingSessions for DatabaseManager (WIP/Untested)

--- SinghxRaj.CodingTracker/DatebaseManager.cs
+++ SinghxRaj.CodingTracker/DatebaseManager.cs
@@ -1,26 +1,81 @@
 ﻿using System.Configuration;
+using Microsoft.Data.Sqlite;
 
 namespace SinghxRaj.CodingTracker;
 
 internal class DatebaseManager
 {
-    private readonly string connectionString = ConfigurationManager.AppSettings.Get("ConnectionString")!;
+    private static string ConnectionString = ConfigurationManager.AppSettings.Get("ConnectionString")!;
+    private const int SucessfullyAddedRow = 1;
 
     public static void CreateTable()
     {
-        // TODO: Fill this in
-        throw new NotImplementedException();
+        string createTable = @"CREATE TABLE IF NOT EXISTS CODING_TRACKER (
+                                      Id INTEGER PRIMARY KEY AUTO INCREMENT
+                                      Start DATEIME,
+                                      End DATETIME,
+                                      Duration INTEGER";
+
+        using (var connection = new SqliteConnection(ConnectionString))
+        {
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = createTable;
+                command.ExecuteNonQuery();
+            }
+
+        }
+
     }
 
     public static bool AddNewCodingSession(CodingSession session)
     {
-        // TODO: Fill this in
-        throw new NotImplementedException();
+        int rowsAdded;
+
+        string start = session.StartTime.ToString("yyyy-MM-dd HH:mm:ss");
+        string end = session.EndTime.ToString("yyyy-MM-dd HH:mm:ss");
+        int durationInSeconds = (int)session.Duration.TotalSeconds;
+
+        string newSession = @$"INSERT INTO CODING_TRACKER (Start, End, Duration)
+                                  VALUES ({ start }, { end }, { durationInSeconds }";
+
+        using (var connection = new SqliteConnection(ConnectionString))
+        {
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = newSession;
+                rowsAdded = command.ExecuteNonQuery();
+            }
+        }
+        return rowsAdded == SucessfullyAddedRow;
+
     }
 
     public static List<CodingSession> GetCodingSessions()
     {
-        // TODO: Fill this in
-        throw new NotImplementedException();
+        string getSessions = @"SELECT * FROM CODING_TRACKER;";
+        var sessions = new List<CodingSession>();
+
+        using (var connection = new SqliteConnection(ConnectionString))
+        {
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = getSessions;
+                var reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    var id = reader.GetInt32(0);
+                    var start = reader.GetDateTime(1);
+                    var end = reader.GetDateTime(2);
+                    int durationInSeconds = reader.GetInt32(3);
+                    var duration = TimeSpan.FromSeconds(durationInSeconds);
+
+                    sessions.Add(new CodingSession(id, start, end, duration));
+                }
+
+            }
+        }
+        return sessions;
     }
 }
