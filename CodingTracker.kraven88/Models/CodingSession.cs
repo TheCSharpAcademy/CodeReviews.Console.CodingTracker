a4ac7b66aea54db0b3c711caf317e1bf1ee69ebf
kraven88 <k.rowecki@gmail.com>
2023-02-03 12:22:38 +0100

    Added CodingSession model

---
+++ CodingTracker.kraven88/Models/CodingSession.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTracker.kraven88.Models;
+
+internal class CodingSession
+{
+    public int Id { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public TimeSpan Duration { get; set; }
+}
