68f574e56bb8b09103159e06d5f269e3d5dee06e
kraven88 <k.rowecki@gmail.com>
2023-02-13 10:28:44 +0100

    Update to SqliteDB class
    - Support for database calls with or without parameters

--- CodingTracker.kraven88/Data/SqliteDB.cs
+++ CodingTracker.kraven88/Data/SqliteDB.cs
@@ -23,11 +23,11 @@ internal class SqliteDB
     {
         SQLiteConnection.CreateFile(DBname);
         var sql =
-            $@"CREATE TABLE ""Sessions""(
-            ""Id"" INTEGER NOT NULL,
+            $@"CREATE TABLE ""Sessions"" (
+            ""Id""     INTEGER NOT NULL,
             ""StartDate"" TEXT NOT NULL,
-            ""EndDate"" TEXT,
-            PRIMATY KEY(""Id"" AUTOINCREMENT)
+            ""EndDate""   TEXT NOT NULL,
+            PRIMARY KEY(""Id"" AUTOINCREMENT)
             );";
 
         SaveData(sql);
@@ -45,6 +45,21 @@ internal class SqliteDB
         }
     }
 
+    public void SaveData(string sqlCommand, CodingSession session)
+    {
+        using (var connection = new SQLiteConnection(LoadConnectionString()))
+        {
+            connection.Open();
+            var sql = connection.CreateCommand();
+            sql.CommandText = sqlCommand;
+            sql.Parameters.AddWithValue("@start", session.Start);
+            sql.Parameters.AddWithValue("@end", session.End);
+
+            sql.ExecuteNonQuery();
+            connection.Close();
+        }
+    }
+
     public List<CodingSession> LoadData(string sqlCommand)
     {
         var output = new List<CodingSession>();
@@ -54,6 +69,36 @@ internal class SqliteDB
             connection.Open();
             var sql = connection.CreateCommand();
             sql.CommandText = sqlCommand;
+            var reader = sql.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    var cs = new CodingSession();
+                    cs.Id = reader.GetInt32(0);
+                    cs.Start = DateTime.Parse(reader.GetString(1));
+                    cs.End = DateTime.Parse(reader.GetString(2));
+                    output.Add(cs);
+                }
+            }
+            reader.Close();
+            connection.Close();
+        }
+
+        return output;
+    }
+
+    public List<CodingSession> LoadData(string sqlCommand, CodingSession session)
+    {
+        var output = new List<CodingSession>();
+
+        using (var connection = new SQLiteConnection(LoadConnectionString()))
+        {
+            connection.Open();
+            var sql = connection.CreateCommand();
+            sql.CommandText = sqlCommand;
+            sql.Parameters.AddWithValue("@start", session.Start);
+            sql.Parameters.AddWithValue("@end", session.End);
             var reader = sql.ExecuteReader();
             if (reader.HasRows)
             {
