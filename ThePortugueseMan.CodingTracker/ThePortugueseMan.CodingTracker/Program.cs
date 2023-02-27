ea62629680a82b3555fcad0f1f19f93f62d84079
ThePortugueseMan <cemartins99@gmail.com>
2023-02-27 13:49:51 +0000

    Initial

---
+++ ThePortugueseMan.CodingTracker/ThePortugueseMan.CodingTracker/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Configuration;
+using System.Collections.Specialized;
+using System.Data.Common;
+
+namespace ThePortugueseMan.CodingTracker;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string? connectionString = ConfigurationManager.AppSettings.Get("defaultConnectionString");
+        DbCommands dbCmd = new(connectionString);
+
+        dbCmd.Initialization();
+
+    }
+}
