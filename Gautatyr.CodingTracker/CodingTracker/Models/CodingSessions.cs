ec9b75d679fe5134b81104399632303e05d9e3ab
Hugo Delautre <97909616+Gautatyr@users.noreply.github.com>
2023-03-30 16:27:40 +0200

    added Gautatyr.CodingTracker

---
+++ Gautatyr.CodingTracker/CodingTracker/Models/CodingSessions.cs
@@ -0,0 +1,9 @@
+﻿namespace CodingTracker.Models;
+
+public class CodingSessions
+{
+    public int Id { get; set; }
+    private DateTime Date { get; set; }
+    public string ShortDate { get; set; }
+    public string TimeSpentCoding { get; set; }
+}
