2d8dcd460c81188e91d88eb7f471f066682f4bc3
barakisbrown <lokislayer@gmail.com>
2023-04-27 21:35:16 -0500

    Model for the application

---
+++ CodingTracker.barakisbrown/CodingTracker.barakisbrown/CodingSession.cs
@@ -0,0 +1,9 @@
+﻿namespace CodingTracker.barakisbrown;
+
+public class CodingSession
+{
+    public int Id { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public DateTime Duration { get; set; }
+}
