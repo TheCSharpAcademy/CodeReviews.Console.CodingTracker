6b307ba175b94d9bee82e04ca5dec45e932793b2
barakisbrown <lokislayer@gmail.com>
2023-05-10 13:04:01 -0500

    Initialize Logger and Setup where Crtl/C can not be used to exit.

--- CodingTracker.barakisbrown/CodingTracker.barakisbrown/Program.cs
+++ CodingTracker.barakisbrown/CodingTracker.barakisbrown/Program.cs
@@ -1,3 +1,16 @@
 ﻿using CodingTracker.barakisbrown;
+using Serilog;
+
+
+// Note : Following will make sure the only way to app will exit is via CRTL|BREAK
+Console.CancelKeyPress += delegate (object? sender, ConsoleCancelEventArgs e)
+{
+    e.Cancel = true;
+};
+// Initialize SeriLog for File Use
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.File("app.log", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
 
 var contol = new CodingController();
