8a853a4697ca2fd44d3d3a1328d754fc9a8b624d
barakisbrown <lokislayer@gmail.com>
2023-05-11 01:31:26 -0500

    Added a menu class to handle all menu choices.

---
+++ CodingTracker.barakisbrown/CodingTracker.barakisbrown/Menu.cs
@@ -0,0 +1,52 @@
+﻿namespace CodingTracker.barakisbrown;
+
+using System;
+
+public class Menu
+{
+    private readonly int[] _menuOptions = new int[] { 0, 1, 2, 3, 4 };
+    private readonly string _menuInputString = "Please Select a menu option or 0 to exit?";
+
+    public void GetMenu()
+    {
+        string menu = @"
+
+            MAIN MENU
+
+            What would you like to do?
+
+            Type 0 to Close Application
+            Type 1 to Add Coding Session
+            Type 2 to Delete Coding Session
+            Type 3 to Update Coding Session
+            Type 4 to Show Completed Coding Session
+            ---------------------------------------
+        ";
+
+        Console.WriteLine(menu);
+    }
+
+    public int GetMenuSelection()
+    {
+        Console.WriteLine(_menuInputString);
+        string? result = Console.ReadLine();
+        int amount;
+
+        while(string.IsNullOrEmpty(result) || !Int32.TryParse(result, out amount) || _menuOptions.Contains(amount))
+        {
+            Console.WriteLine("Your answer need to be an acceptable result");
+            Console.WriteLine(_menuInputString);
+            result = Console.ReadLine();
+        }
+        return amount;
+    }
+
+    public static bool GetYesNo()
+    {
+        ConsoleKeyInfo input = Console.ReadKey(true);
+        if (input.Key == ConsoleKey.Y)
+            return true;
+        else 
+            return false;
+    }
+}
