eb14f7f01bb2c311f767f30fcecb4582407d6ae8
barakisbrown <lokislayer@gmail.com>
2023-05-12 01:34:11 -0500

    Added Proper Menu Flow for the App

--- CodingTracker.barakisbrown/CodingTracker.barakisbrown/Program.cs
+++ CodingTracker.barakisbrown/CodingTracker.barakisbrown/Program.cs
@@ -13,4 +13,50 @@ Log.Logger = new LoggerConfiguration()
     .WriteTo.File("app.log", rollingInterval: RollingInterval.Day)
     .CreateLogger();
 
-var contol = new CodingController();
+var menu = new Menu(new CodingController());
+int option = -1;
+
+while(option != 0)
+{
+    Console.WriteLine("Welcome to Coding Session. This will be tracking your coding session.");
+    menu.GetMenu();
+    option = menu.GetMenuSelection();
+
+    switch (option)
+    {
+        case 0:
+            break;
+        case 1:
+            AddSession();
+            break;
+        case 2:
+            DeleteSession();
+            break;
+        case 3:
+            UpdateSession();
+            break;
+        case 4:
+            ShowAllSessions();
+            break;
+    }
+}
+
+void ShowAllSessions()
+{
+    throw new NotImplementedException();
+}
+
+void UpdateSession()
+{
+    throw new NotImplementedException();
+}
+
+void DeleteSession()
+{
+    throw new NotImplementedException();
+}
+
+void AddSession()
+{
+    throw new NotImplementedException();
+}
