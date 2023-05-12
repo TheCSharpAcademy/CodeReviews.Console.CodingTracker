8900e0d90d07776270f25c229daf5c3b073141f3
Corey Jordan <113132164+CoreyJordan@user.noreply.github.com>
2023-05-12 18:35:22 -0400

    Add App.config and connection string
    Created the configuration file that contains the connection
    string as well as created a CodingSessionModel class.
    This class is empty thus far.
    Created the library and the CrudController class
    in order to test connection string.

--- CodingTracker.CoreyJordan/CodingTracker.CoreyJordan/Program.cs
+++ CodingTracker.CoreyJordan/CodingTracker.CoreyJordan/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CodingTrackerLibrary;
+
+try
+{
+    CrudController.InitDatabase();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
---
+++ CodingTracker.CoreyJordan/CodingTrackerLibrary/CrudController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Data.Sqlite;
+using System.Configuration;
+
+namespace CodingTrackerLibrary;
+public static class CrudController
+{
+    private static string ConnString(string name)
+    {
+        return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+    }
+
+    public static void InitDatabase()
+    {
+        using (var connection = new SqliteConnection(ConnString("CodingDb")))
+        {
+            connection.Open();
+            var command = connection.CreateCommand();
+
+            command.CommandText =
+                @$"CREATE TABLE IF NOT EXISTS CodingSession
+                (Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                StartTime TEXT,
+                EndTime TEXT,
+                Duration TEXT)";
+
+            command.ExecuteNonQuery();
+        }
+    }
+}
