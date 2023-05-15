bb1c15fc95b1ff05c9f07791527eba8dba7f1eb4
barakisbrown <lokislayer@gmail.com>
2023-05-15 01:25:17 -0500

    Refactored where all the menu code is in the menu class

--- CodingTracker.barakisbrown/CodingTracker.barakisbrown/Program.cs
+++ CodingTracker.barakisbrown/CodingTracker.barakisbrown/Program.cs
@@ -1,7 +1,6 @@
 ﻿using CodingTracker.barakisbrown;
 using Serilog;
 
-
 // Note : Following will make sure the only way to app will exit is via CRTL|BREAK
 Console.CancelKeyPress += delegate (object? sender, ConsoleCancelEventArgs e)
 {
@@ -13,65 +12,4 @@ Log.Logger = new LoggerConfiguration()
     .WriteTo.File("app.log", rollingInterval: RollingInterval.Day)
     .CreateLogger();
 
-var menu = new Menu(new CodingController());
-int option = -1;
-
-while(option != 0)
-{
-    Console.WriteLine("Welcome to Coding Session. This will be tracking your coding session.");
-    menu.GetMenu();
-    option = menu.GetMenuSelection();
-
-    switch (option)
-    {
-        case 0:
-            break;
-        case 1:
-            AddSession();
-            break;
-        case 2:
-            DeleteSession();
-            break;
-        case 3:
-            UpdateSession();
-            break;
-        case 4:
-            ShowAllSessions();
-            break;
-    }
-}
-
-void ShowAllSessions()
-{
-    Console.Clear();
-    Console.WriteLine("Delete a Session");
-
-    menu.GetKeyReturnMenu();
-}
-
-void UpdateSession()
-{
-    Console.Clear();
-    Console.WriteLine("Delete a Session");
-
-    menu.GetKeyReturnMenu();
-}
-
-    void DeleteSession()
-{
-    Console.Clear();
-    Console.WriteLine("Delete a Session");
-
-    menu.GetKeyReturnMenu();
-}
-
-void AddSession()
-{
-    Console.Clear();
-    Console.WriteLine("Adding Session.\n");
-
-    
-
-
-    menu.GetKeyReturnMenu();
-}
+var _ = new Menu(new CodingController());
