0f11abf8a2b38b54bc4196394a2c764ac978fa9c
barakisbrown <lokislayer@gmail.com>
2023-05-17 12:54:25 -0500

    Moved validation from menu to here.

--- CodingTracker.barakisbrown/CodingTracker.barakisbrown/CodingSession.cs
+++ CodingTracker.barakisbrown/CodingTracker.barakisbrown/CodingSession.cs
@@ -25,8 +25,60 @@ public class CodingSession
         Duration = CalculateDuration(begin.Time, end.Time);
     }
 
-    public DTSeperated SeperateBegin() { throw new NotImplementedException(); }
-    public DTSeperated SeperateEnd() { throw new NotImplementedException(); }
+    public DTSeperated SeperateBegin() 
+    {
+        return new()
+        {
+            Date = DateOnly.FromDateTime(StartTime),
+            Time = TimeOnly.FromDateTime(EndTime)
+        };
+    }
+    public DTSeperated SeperateEnd() 
+    {
+        return new()
+        {
+            Date = DateOnly.FromDateTime(EndTime),
+            Time = TimeOnly.FromDateTime(EndTime)
+        };
+    }
+
+    public void ValidateTime(DTSeperated begin, DTSeperated end)
+    {
+        // NEED TO MAKE SURE THE BEGIN TIME IS NOT GREATER THAN THE END TIME WHILE DATE IS THE SAME
+        // TEST IF BEGIN > END AND END LESS THAN BEGIN 
+        var exitFlag = true;
+        while (exitFlag)
+        {
+            if ((begin.Time >= end.Time) && (begin.Date == end.Date))
+            {
+                Console.WriteLine("\nBegin time can not be equal or greater than the end time.  Please re-enter the begin time.");
+                begin.Time = Input.GetTime();
+            }
+            else if (end.Time < begin.Time && end.Date == begin.Date)
+            {
+                Console.WriteLine("\nEnd Time can not be less than the begin time.");
+                Console.WriteLine("Please re-enter the end time");
+                end.Time = Input.GetTime();
+            }
+            else
+                exitFlag = false;
+        }
+    }
+
+    public void ValidateDate(DTSeperated begin, DTSeperated end)
+    {
+        var exitFlag = true;
+        while (exitFlag)
+        {
+            if (end.Date < begin.Date)
+            {
+                Console.WriteLine("\nYou can not have a date in the past for the end session. Please try again.");
+                end.Date = Input.GetDate();
+            }
+            else
+                exitFlag = false;
+        }
+    }
 }
 
 public struct DTSeperated
