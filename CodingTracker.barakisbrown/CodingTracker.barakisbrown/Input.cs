50916561291506e09bb6da6ae26bf3de5f7b6a53
barakisbrown <lokislayer@gmail.com>
2023-05-18 18:36:25 -0500

    Fixed GetUpdateOptions and overloaded GetTime/Get Date

--- CodingTracker.barakisbrown/CodingTracker.barakisbrown/Input.cs
+++ CodingTracker.barakisbrown/CodingTracker.barakisbrown/Input.cs
@@ -11,6 +11,8 @@ public static class Input
     private readonly static string _timeInputString = $"Enter the time in the following format [{_validTimeFormat}] or Enter for current time  :>";
     private readonly static string _deleteInputString = "Enter ID of the Session you would like deleted or -1 to exit |> ";
     private readonly static string _updateInputString = "Enter ID of the Session you would like updated or -1 to exit |> ";
+    private readonly static string _updatedDateInput = $"Enter the date in the following format [{_validDateFormat}] or Enter for no changes:>";
+    private readonly static string _updatedTimeInput = $"Enter the time in the following format [{_validTimeFormat}] or Enter for no changes  :>";
 
     public static bool GetYesNo()
     {
@@ -23,21 +25,26 @@ public static class Input
 
     public static char GetUpdateOptions()
     {
-        ConsoleKeyInfo input = Console.ReadKey(true);
-        char keyReturned = ' ';
-        switch(input.Key)
+        while (true)
         {
-            case ConsoleKey.S:
-                keyReturned = 'S';
-                break;
-            case ConsoleKey.E:
-                keyReturned = 'E';
-                break;
-            case ConsoleKey.C:
-                keyReturned = 'C';
-                break;
-        }
-        return keyReturned;
+            ConsoleKeyInfo input = Console.ReadKey(true);
+            char keyReturned;
+            switch (input.Key)
+            {
+                case ConsoleKey.S:
+                    keyReturned = 'S';
+                    break;
+                case ConsoleKey.E:
+                    keyReturned = 'E';
+                    break;
+                case ConsoleKey.C:
+                    keyReturned = 'C';
+                    break;
+                default:
+                    continue;                    
+            }
+            return keyReturned;
+        }        
     }
 
     public static void GetKeyReturnMenu()
@@ -57,6 +64,31 @@ public static class Input
         {
             if (result == string.Empty)
                 return DateOnly.FromDateTime(DateTime.Now);
+            try
+            {
+                DateTime.TryParseExact(result, _validDateFormat, new CultureInfo("en-us"), DateTimeStyles.None, out DateTime date);
+                return DateOnly.FromDateTime(date);
+            }
+            catch (FormatException _)
+            {
+                Log.Error("F> GetDate() raised exception and caught. Exception Message : {0}", _.Message);
+                Console.WriteLine($"Date has to in the following format: {_validDateFormat} ");
+                Console.WriteLine("Please try again.");
+                Console.WriteLine(_dateInputString);
+                result = Console.ReadLine();
+            }
+        }
+    }
+
+    public static DateOnly GetDate(DateOnly currentDate)
+    {
+        Console.WriteLine(_updatedDateInput);
+        string? result = Console.ReadLine();
+
+        while (true)
+        {
+            if (result == string.Empty)
+                return currentDate;
             try
             {
                 DateTime.TryParseExact(result, _validDateFormat, new CultureInfo("en-us"), DateTimeStyles.None, out DateTime date);
@@ -137,6 +169,71 @@ public static class Input
                 Console.WriteLine("\nOkay. Lets try it again.");
         }
     }
+
+    public static TimeOnly GetTime(TimeOnly currentTime)
+    {
+        while (true)
+        {
+            Console.Write(_updatedTimeInput);
+            string? result = Console.ReadLine();
+            int hourInt = 0, minuteInt = 0;
+
+            if (result == string.Empty)
+            {
+                return currentTime;
+            }
+            else if (!result.Contains(':'))
+            {
+                Log.Debug("F> GetTime() -- User entered wrong information. Will be told to reenter.");
+                Console.WriteLine("Invalid Time Entered. It must be HH:MM");
+            }
+            else
+            {
+                var parse = result?.Split(":");
+                try
+                {
+                    hourInt = int.Parse(parse[0]);
+                    if (hourInt >= 24)
+                    {
+                        Log.Debug("F> GetTime() -- User entered wrong information. Will be told to reenter.");
+                        Console.WriteLine("Invalid Hour : Hour should be between 0 and 24");
+                        continue;
+                    }
+                }
+                catch (FormatException _)
+                {
+                    Log.Error("F> GetTime() raised an exception and caught. Exception message is {0}", _.Message);
+                    Console.WriteLine("Invalid Information. Please make sure it is numerical.");
+                    continue;
+                }
+                try
+                {
+                    minuteInt = int.Parse(parse[1]);
+                    if (minuteInt >= 60)
+                    {
+                        Log.Debug("F> GetTime() -- User entered wrong information. Will be told to reenter.");
+                        Console.WriteLine("Invalid time. Time should be between 0 and 60.");
+                        continue;
+                    }
+                }
+                catch (FormatException _)
+                {
+                    Log.Error("F> GetTime() raised an exception and caught. Exception message is {0}", _.Message);
+                    Console.WriteLine("Invalid Information. Please make sure it is numerical.");
+                    continue;
+                }
+            }
+            // Confirm if this is the correct time entered.
+            TimeOnly retTime = new(hourInt, minuteInt);
+
+            Console.Write($"Did you enter {retTime}  (Y/N)?");
+            if (GetYesNo())
+                return retTime;
+            else
+                Console.WriteLine("\nOkay. Lets try it again.");
+        }
+    }
+
 
     public static int GetNumberFromList(List<int> validIds, bool deleted)
     {
