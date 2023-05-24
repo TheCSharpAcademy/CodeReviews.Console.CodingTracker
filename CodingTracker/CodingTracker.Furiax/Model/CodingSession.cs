7bd12777be25c5d1386d68c7a234c60886292e79
furiax <carlmalfliet@proximus.be>
2023-05-24 15:12:15 +0200

    created configfile + creating db and table

---
+++ CodingTracker/CodingTracker.Furiax/Model/CodingSession.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTracker.Furiax.Model
+{
+	internal class CodingSession
+	{
+        public int Id { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public TimeSpan Duration => EndTime - StartTime;
+    }
+}
---
+++ CodingTracker/CodingTracker.Furiax/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Data.Sqlite;
+using System.Configuration;
+
+var connectionString = ConfigurationManager.AppSettings.Get("connectionString");
+
+using(var connection = new SqliteConnection(connectionString))
+{
+	connection.Open();
+	var command = connection.CreateCommand();
+	command.CommandText = @"CREATE TABLE IF NOT EXISTS CodeTracker(
+							Id INTEGER PRIMARY KEY AUTOINCREMENT,
+							StartTime TEXT NOT NULL,
+							EndTime TEXT NOT NULL,
+							Duration TEXT)";
+	command.ExecuteNonQuery();
+	connection.Close();
+}
