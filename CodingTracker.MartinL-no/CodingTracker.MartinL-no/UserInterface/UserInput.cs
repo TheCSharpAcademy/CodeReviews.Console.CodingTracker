4d974e62e1b421cbbeba6b70b4cbad33bc43bd95
Martin <102468994+MartinL-no@users.noreply.github.com>
2023-08-08 14:50:55 +0200

    feature[UserInput]: create class

---
+++ CodingTracker.MartinL-no/CodingTracker.MartinL-no/UserInterface/UserInput.cs
@@ -0,0 +1,13 @@
+﻿using CodingTracker.MartinL_no.Controllers;
+
+namespace CodingTracker.MartinL_no.UserInterface;
+
+internal class UserInput
+{
+    private readonly CodingController _controller;
+
+    public UserInput(CodingController controller)
+	{
+        _controller = controller;
+    }
+}
