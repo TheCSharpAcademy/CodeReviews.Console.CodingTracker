e20ce3e91a9067cf1d97a5b47b11d7d4a104baf1
Martin <102468994+MartinL-no@users.noreply.github.com>
2023-08-08 10:23:22 +0200

    feat: create App.config

---
+++ CodingTracker.MartinL-no/CodingTracker.MartinL-no/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Configuration;
+using System.Collections.Specialized;
+
+var dbPath = ConfigurationManager.AppSettings.Get("DbPath");
+var connString = ConfigurationManager.AppSettings.Get("ConnString");
+
+Console.WriteLine(connString + dbPath);
