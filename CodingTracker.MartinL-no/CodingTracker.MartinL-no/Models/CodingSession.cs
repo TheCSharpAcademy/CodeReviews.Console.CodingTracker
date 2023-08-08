60b4d903ce703730d9bcfa1ada4909b257977d0d
Martin <102468994+MartinL-no@users.noreply.github.com>
2023-08-08 10:35:38 +0200

    feat: create CodingSession.cs model

---
+++ CodingTracker.MartinL-no/CodingTracker.MartinL-no/Models/CodingSession.cs
@@ -0,0 +1,14 @@
+﻿namespace CodingTracker.MartinL_no.Models;
+
+internal class CodingSession
+{
+    internal DateTime StartTime { get; set; }
+    internal DateTime EndTime { get; set; }
+    internal TimeSpan Duration => EndTime - StartTime;
+
+	internal CodingSession(DateTime startTime, DateTime endTime)
+	{
+        StartTime = startTime;
+        EndTime = endTime;
+    }
+}
