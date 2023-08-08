0e16fa92a1aba0e278dfa7acd1bf980b04911f42
Martin <102468994+MartinL-no@users.noreply.github.com>
2023-08-08 11:06:14 +0200

    feature: Create database and CodingSession table

---
+++ CodingTracker.MartinL-no/CodingTracker.MartinL-no/DAL/CodingSessionRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace CodingTracker.MartinL_no.DAL;
+
+internal class CodingSessionRepository
+{
+    private readonly string ConnString;
+    private readonly string DbName;
+
+	internal CodingSessionRepository(string connString, string dbName)
+	{
+        ConnString = connString;
+        DbName = dbName;
+        CreateTable();
+    }
+
+    private void CreateTable()
+    {
+        using (var connection = new SqliteConnection($"{ConnString}{DbName}"))
+        {
+            connection.Open();
+
+            var command = connection.CreateCommand();
+            command.CommandText = """
+                CREATE TABLE IF NOT EXISTS CodingSession (
+                    Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                    StartTime TEXT NOT NULL,
+                    EndTime TEXT NOT NULL
+                );
+                """;
+            command.ExecuteNonQuery();
+        }
+    }
+}
--- CodingTracker.MartinL-no/CodingTracker.MartinL-no/Program.cs
+++ CodingTracker.MartinL-no/CodingTracker.MartinL-no/Program.cs
@@ -1,12 +1,8 @@
 ﻿using System.Configuration;
 
-using CodingTracker.MartinL_no.Models;
+using CodingTracker.MartinL_no.DAL;
 
 var dbPath = ConfigurationManager.AppSettings.Get("DbPath");
 var connString = ConfigurationManager.AppSettings.Get("ConnString");
 
-Console.WriteLine(connString + dbPath);
-
-var start = DateTime.Now.AddDays(1);
-var end = DateTime.Now.AddDays(2);
-var session = new CodingSession(start, end);
+var repo = new CodingSessionRepository(connString, dbPath);
