2f66a744b50ee99e379a5557429aec8e51e7d44a
devhawkz <skydevhawkz@gmail.com>
2023-08-24 20:01:14 +0200

    initial commit

---
+++ CodingTracker.DEVHAWKZ/CodingTracker.library/Controller/QueriesCodingSessionGoal.cs
@@ -0,0 +1,38 @@
+﻿using System.Configuration;
+using Microsoft.Data.Sqlite;
+
+namespace CodingTracker.library.Controller;
+
+internal static class QueriesCodingSessionGoal
+{
+    private static string connectionString = ConfigurationManager.AppSettings.Get("connectionString");
+
+    internal static double TotalLastWeekCodingHoursQuery(string startDate, string endDate)
+    {
+        double totalHours = 0;
+
+        using (SqliteConnection connection = new SqliteConnection(connectionString))
+        {
+            connection.Open();
+
+            var command = connection.CreateCommand();
+            
+            command.CommandText = $@"
+                SELECT Duration
+                FROM sessions
+                WHERE datetime(StartTime) >= datetime('{startDate}')
+                AND datetime(EndTime) <= datetime('{endDate}');";
+
+            SqliteDataReader reader = command.ExecuteReader();
+            
+
+            while (reader.Read())
+            {
+                totalHours = reader.GetDouble(0);
+            }   
+            
+        }
+
+        return totalHours;
+    }
+}
