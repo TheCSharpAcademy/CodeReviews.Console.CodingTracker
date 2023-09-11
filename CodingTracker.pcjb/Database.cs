6c20e17c743bc13684ca8e101ad99ef62a505524
pcjb <143715460+pcjb@users.noreply.github.com>
2023-09-11 17:44:41 +0200

    Database CRUD coding sessions

--- CodingTracker.pcjb/Database.cs
+++ CodingTracker.pcjb/Database.cs
@@ -15,6 +15,142 @@ class Database
         this.databaseFilename = databaseFilename;
     }
 
+    public bool CreateCodingSession(CodingSession session)
+    {
+        try
+        {
+            using var connection = new SqliteConnection(GetConnectionString());
+            connection.Open();
+            var command = connection.CreateCommand();
+            command.CommandText =
+            @"
+            INSERT INTO coding_sessions
+            (start, end, duration) 
+            VALUES 
+            ($start, $end, $duration)
+            ";
+            command.Parameters.AddWithValue("$start", session.StartTime);
+            command.Parameters.AddWithValue("$end", session.EndTime);
+            command.Parameters.AddWithValue("$duration", session.Duration.TotalSeconds);
+            return command.ExecuteNonQuery() == 1;
+        }
+        catch (Exception ex)
+        {
+            Logger.Error(ex);
+            return false;
+        }
+    }
+
+    public CodingSession? ReadCodingSession(long id)
+    {
+        CodingSession? session = null;
+        try
+        {
+            using var connection = new SqliteConnection(GetConnectionString());
+            connection.Open();
+            var command = connection.CreateCommand();
+            command.CommandText =
+            @"
+            SELECT start, end, duration 
+            FROM coding_sessions
+            WHERE id = $id
+            ";
+            command.Parameters.AddWithValue("$id", id);
+            using var reader = command.ExecuteReader();
+            if (reader.Read())
+            {
+                var start = reader.GetDateTime(0);
+                var end = reader.GetDateTime(1);
+                var duration = TimeSpan.FromSeconds(reader.GetDouble(2));
+                session = new CodingSession(id, start, end, duration);
+            }
+        }
+        catch (Exception ex)
+        {
+            Logger.Error(ex);
+        }
+        return session;
+    }
+
+    public List<CodingSession> ReadAllCodingSessions()
+    {
+        List<CodingSession> sessions = new();
+        try
+        {
+            using var connection = new SqliteConnection(GetConnectionString());
+            connection.Open();
+            var command = connection.CreateCommand();
+            command.CommandText =
+            @"
+            SELECT id, start, end, duration 
+            FROM coding_sessions
+            ORDER BY start ASC
+            ";
+            using var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                var id = reader.GetInt64(0);
+                var start = reader.GetDateTime(1);
+                var end = reader.GetDateTime(2);
+                var duration = TimeSpan.FromSeconds(reader.GetDouble(3));
+                sessions.Add(new CodingSession(id, start, end, duration));
+            }
+        }
+        catch (Exception ex)
+        {
+            Logger.Error(ex);
+        }
+        return sessions;
+    }
+
+    public bool UpdateCodingSession(CodingSession session)
+    {
+        try
+        {
+            using var connection = new SqliteConnection(GetConnectionString());
+            connection.Open();
+            var command = connection.CreateCommand();
+            command.CommandText =
+            @"
+            UPDATE coding_sessions
+            SET start=$start, end=$end, duration=$duration
+            WHERE id=$id
+            ";
+            command.Parameters.AddWithValue("$id", session.Id);
+            command.Parameters.AddWithValue("$start", session.StartTime);
+            command.Parameters.AddWithValue("$end", session.EndTime);
+            command.Parameters.AddWithValue("$duration", session.Duration.TotalSeconds);
+            return command.ExecuteNonQuery() == 1;
+        }
+        catch (Exception ex)
+        {
+            Logger.Error(ex);
+            return false;
+        }
+    }
+
+    public bool DeleteCodingSession(long id)
+    {
+        try
+        {
+            using var connection = new SqliteConnection(GetConnectionString());
+            connection.Open();
+            var command = connection.CreateCommand();
+            command.CommandText =
+            @"
+            DELETE FROM coding_sessions
+            WHERE id=$id
+            ";
+            command.Parameters.AddWithValue("$id", id);
+            return command.ExecuteNonQuery() == 1;
+        }
+        catch (Exception ex)
+        {
+            Logger.Error(ex);
+            return false;
+        }
+    }
+
     public bool CreateDatabaseIfNotPresent()
     {
         try
