67454e3b64ea45765d66ad2b8a47e0c101de3069
pcjb <143715460+pcjb@users.noreply.github.com>
2023-09-11 12:56:10 +0200

    Add model for a coding session

---
+++ CodingTracker.pcjb/CodingSession.cs
@@ -0,0 +1,29 @@
+namespace CodingTracker;
+
+class CodingSession
+{
+    public long Id { get; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public TimeSpan Duration { get; }
+   
+    public CodingSession(long id, DateTime start, DateTime end, double duration)
+    {
+        Id = id;
+        StartTime = start;
+        EndTime = end;
+        Duration = duration;
+    }
+
+    public CodingSession(DateTime start, DateTime end)
+    {
+        StartTime = start;
+        EndTime = end;
+        Duration = CalculateDuration();
+    }
+
+    private TimeSpan CalculateDuration()
+    {
+        return EndTime - StartTime;
+    }
+}
