8e8fa93f13ae682c8e1158e8a3c8e4c752f3d255
Stan Choi <sjc591@nyu.edu>
2023-09-27 10:23:41 -0400

    Make config file, try to get connection string in main program

---
+++ StanChoi.CodingTracker/StanChoi.CodingTracker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Configuration;
+
+namespace coding_tracker
+{
+    static string connectionString = ConfigurationManager.AppSettings.Get("ConnectionString");
+
+    static void Main(string[] args)
+    {
+
+    }
+}
