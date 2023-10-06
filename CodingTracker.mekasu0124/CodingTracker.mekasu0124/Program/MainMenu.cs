8e8a1a9f15946f9574adcf1ccf3428395c4ab888
Mekasu <96821081+mekasu0124@users.noreply.github.com>
2023-10-05 23:31:48 -0500

    removed comments

--- CodingTracker.mekasu0124/CodingTracker.mekasu0124/Program/MainMenu.cs
+++ CodingTracker.mekasu0124/CodingTracker.mekasu0124/Program/MainMenu.cs
@@ -9,8 +9,8 @@ public class MainMenu
     {
         List<string> menuOptions = new()
         {
-            "1 - New Coding Session", // works
-            "2 - Edit Coding Session", // works
+            "1 - New Coding Session",
+            "2 - Edit Coding Session",
             "3 - Delete Coding Session",
             "4 - View All Sessions",
             "5 - New Goal",
@@ -20,8 +20,6 @@ public class MainMenu
             "0 - Exit Program"
         };
 
-        Console.WriteLine($"Welcome To Your Coding Tracker!");
-        Console.WriteLine($"Current Date: {Helpers.GetDateTime()}");
         Console.WriteLine("\nWhat Would You Like To Do?");
 
         foreach (string option in menuOptions)
@@ -36,29 +34,58 @@ public class MainMenu
         switch (input)
         {
             case "1":
-                CRUDController.NewSession();
+                Console.WriteLine("\nHow Would You Like To Record This Session?");
+                Console.WriteLine("1 - Manual Time Entry Session");
+                Console.WriteLine("2 - Automated Stopwatch Session");
+                Console.Write("\nYour Selection: ");
+
+                input = Console.ReadLine();
+                input = UserValidation.ValidateSecondaryMenu(input);
+
+                switch (input)
+                {
+                    case "1":
+                        CRUDSessionController.NewSessionManual();
+                        break;
+
+                    case "2":
+                        CRUDSessionController.NewSessionStopWatch();
+                        break;
+
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("[Error] Couldn't Determine Session Start Type. Relaunching Main Menu");
+                        ShowMenu();
+                        break;
+                }
                 break;
 
             case "2":
-                CRUDController.EditSession();
+                CRUDSessionController.EditSession();
                 break;
 
             case "3":
+                CRUDSessionController.DeleteSession();
                 break;
 
             case "4":
+                CRUDSessionController.ViewAllCodingSessions();
                 break;
 
             case "5":
+                CRUDSessionController.NewGoal();
                 break;
 
             case "6":
+                CRUDSessionController.EditGoal();
                 break;
 
             case "7":
+                CRUDSessionController.DeleteGoal();
                 break;
 
             case "8":
+                CRUDSessionController.ViewAllGoals();
                 break;
 
             case "0":
