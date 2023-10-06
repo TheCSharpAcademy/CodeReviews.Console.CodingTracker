9081acebe57e9222dc4f7888a08a252a7c42f629
Mekasu <96821081+mekasu0124@users.noreply.github.com>
2023-10-05 23:29:22 -0500

    fixed null possiblities

--- CodingTracker.mekasu0124/CodingTracker.mekasu0124/Models/CodeSessionModel.cs
+++ CodingTracker.mekasu0124/CodingTracker.mekasu0124/Models/CodeSessionModel.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CodingTracker.Models;
+﻿namespace CodingTracker.Models;
 
 public class CodeSession
 {
-    public int Id { get; set; }
-    public string TodaysDate { get; set; }
-    public string StartTime { get; set; }
-    public string EndTime { get; set; }
-    public string Duration { get; set; }
+    public int? Id { get; set; }
+    public string? TodaysDate { get; set; }
+    public string? StartTime { get; set; }
+    public string? EndTime { get; set; }
+    public string? Duration { get; set; }
 }
