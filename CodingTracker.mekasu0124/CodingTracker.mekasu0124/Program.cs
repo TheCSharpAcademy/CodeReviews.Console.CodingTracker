a39540483cdce89987c74fcbbb429060933fb1f7
Mekasu <96821081+mekasu0124@users.noreply.github.com>
2023-10-05 23:33:53 -0500

    fixed program file

--- CodingTracker.mekasu0124/CodingTracker.mekasu0124/Program.cs
+++ CodingTracker.mekasu0124/CodingTracker.mekasu0124/Program.cs
@@ -1,20 +1,25 @@
-﻿using System.Configuration;
-using CodingTracker.Services;
+﻿using CodingTracker.Services;
 
 namespace CodingTracker;
 
 public class CodingTracker
 {
-    private static readonly string dbFile = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
+    private static readonly string dbFile = "coding_logs.db";
     public static void Main(string[] args)
     {
+        Console.WriteLine($"Welcome To Your Coding Tracker!");
+        Console.WriteLine($"Current Date: {Helpers.GetDate(false)}");
+        Thread.Sleep(2000);
+
         if (!File.Exists(dbFile))
         {
             Database.CreateDatabase();
+            Console.Clear();
             MainMenu.ShowMenu();
         }
         else
         {
+            Console.Clear();
             MainMenu.ShowMenu();
         }
     }
