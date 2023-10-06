a0d4da9136ce30ab443e0a5d8bc766189d5c6e29
Mekasu <96821081+mekasu0124@users.noreply.github.com>
2023-10-05 23:29:36 -0500

    fixed null possibilities

---
+++ CodingTracker.mekasu0124/CodingTracker.mekasu0124/Models/GoalModel.cs
@@ -0,0 +1,12 @@
+﻿namespace CodingTracker.Models;
+
+public class Goal
+{
+    public int? Id { get; set; }
+    public string? Name { get; set; }
+    public string? StartDate { get; set; }
+    public string? EndDate { get; set; }
+    public double? DaysToGoal { get; set; }
+    public int? HoursPerDay { get; set; }
+    public string? Achieved { get; set; }
+}
