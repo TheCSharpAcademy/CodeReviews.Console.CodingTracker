690165a89c6cc09b24a4188181ca8fd5ee088fb1
Mekasu <96821081+mekasu0124@users.noreply.github.com>
2023-10-05 23:33:02 -0500

    created table visualization engine

---
+++ CodingTracker.mekasu0124/CodingTracker.mekasu0124/Services/TableVisualizationEngine.cs
@@ -0,0 +1,15 @@
+﻿using ConsoleTableExt;
+
+namespace CodingTracker.Services;
+
+public class TableVisualizationEngine
+{
+    public static void ShowTable(List<List<object?>?>? tableData, List<string?>? columnHeaders, string? title)
+    {
+        ConsoleTableBuilder
+            .From(tableData)
+            .WithTitle(title)
+            .WithColumn(columnHeaders)
+            .ExportAndWrite();
+    }
+}
