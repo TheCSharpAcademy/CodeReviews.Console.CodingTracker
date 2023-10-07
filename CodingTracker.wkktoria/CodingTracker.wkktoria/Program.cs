fc0356065e3fd2344530607f64a53e6d06938bc8
wkktoria <127852624+wkktoria@users.noreply.github.com>
2023-10-07 15:20:52 +0200

    Initialize database

--- CodingTracker.wkktoria/CodingTracker.wkktoria/Program.cs
+++ CodingTracker.wkktoria/CodingTracker.wkktoria/Program.cs
@@ -1,6 +1,11 @@
 ﻿using System.Configuration;
+using CodingTracker.wkktoria;
 
 var pathToSaveDb = Path.GetDirectoryName(Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent
     .FullName);
 var dbPath = Path.Combine(pathToSaveDb, ConfigurationManager.AppSettings.Get("DbPath"));
 var connectionString = ConfigurationManager.AppSettings.Get("ConnectionString") + dbPath;
+
+var dbManager = new DbManager(connectionString);
+
+dbManager.Initialize();
