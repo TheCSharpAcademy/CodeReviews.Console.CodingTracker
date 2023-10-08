82f4c9349bcd80f674a9f8ae629347589ed5e70e
Andreas Abaidoo <abaidooandy@gmail.com>
2023-10-08 08:30:06 +0000

    Created CodingSession Model

---
+++ CodingTracker.AndreasGuy54/CodingTracker.AndreasGuy54/CodingSession.cs
@@ -0,0 +1,17 @@
+﻿namespace CodingTracker.AndreasGuy54
+{
+    internal class CodingSession
+    {
+        public int Id { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        private TimeSpan myTime;
+
+        public TimeSpan Duration
+        {
+            get { return myTime; }
+            set { myTime = EndTime - StartTime; }
+        }
+    }
+}
