27b64514d9e472ff52eee2e2f255c88a945f152e
David Nyman <david.nyman@protonmail.com>
2023-10-16 14:16:28 +0200

    Add complete project

---
+++ CodingTracker.dnym/CodingTracker/UI/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using CodingTracker.DataAccess;
+using TCSAHelper.Console;
+
+namespace CodingTracker.UI;
+
+internal static class MainMenu
+{
+    internal static Screen Get(IDataAccess dataAccess)
+    {
+        var screen = new Screen(
+            header: (_, _) => "Coding Tracker",
+            body: (_, _) => @"1. Log Coding Session
+2. Manage Coding Session Logs
+0. Quit",
+            footer: (_, _) => "Press a number to select.");
+        screen.AddAction(ConsoleKey.D1, () => SessionLoggingScreen.Get(dataAccess).Show());
+        screen.AddAction(ConsoleKey.NumPad1, () => SessionLoggingScreen.Get(dataAccess).Show());
+        screen.AddAction(ConsoleKey.D2, () => LogManagementScreen.Get(dataAccess).Show());
+        screen.AddAction(ConsoleKey.NumPad2, () => LogManagementScreen.Get(dataAccess).Show());
+        screen.AddAction(ConsoleKey.D0, () => screen.ExitScreen());
+        screen.AddAction(ConsoleKey.NumPad0, () => screen.ExitScreen());
+        return screen;
+    }
+}
---
+++ CodingTracker.dnym/CodingTracker/UI/SingleLogViewScreen.cs
@@ -0,0 +1,85 @@
+﻿using CodingTracker.DataAccess;
+using CodingTracker.Models;
+using TCSAHelper.Console;
+
+namespace CodingTracker.UI;
+
+internal static class SingleLogViewScreen
+{
+    internal static Screen Get(IDataAccess dataAccess, int id)
+    {
+        var session = dataAccess.Get(id);
+        if (session == null)
+        {
+            return GetErrorScreen();
+        }
+        else
+        {
+            return GetScreen(dataAccess, id);
+        }
+    }
+
+    private static Screen GetErrorScreen()
+    {
+        static string body(int _1, int _2)
+        {
+            return "There is no coding session with that id.\n\nPress any key to return.";
+        }
+        var screen = new Screen(body: body);
+        screen.SetAnyKeyAction(() => screen.ExitScreen());
+        return screen;
+    }
+
+    private static Screen GetScreen(IDataAccess dataAccess, int id)
+    {
+        static string header(int _1, int _2)
+        {
+            return "Viewing Coding Session";
+        }
+
+        static string footer(int _1, int _2)
+        {
+            return "Press [M] to modify the session,\n[D] to delete,\nor [Esc] to go back to the main menu.";
+        }
+
+        string body(int _1, int _2)
+        {
+            var session = dataAccess.Get(id)!;
+            string start = session.StartTime.ToLocalTime().ToString(Program.mainFullFormat);
+            string end = session.EndTime.ToLocalTime().ToString(Program.mainFullFormat);
+            string duration = DurationString(session);
+            return $"Start: {start}\nEnd: {end}\nDuration: {duration}";
+        }
+
+        var screen = new Screen(header: header, body: body, footer: footer);
+        screen.AddAction(ConsoleKey.Escape, () => screen.ExitScreen());
+        screen.AddAction(ConsoleKey.M, () =>
+        {
+            var session = dataAccess.Get(id)!;
+            var modScreen = SessionLoggingScreen.Get(dataAccess, session);
+            modScreen.Show();
+        });
+        screen.AddAction(ConsoleKey.D, () =>
+        {
+            dataAccess.Delete(id);
+            screen.ExitScreen();
+        });
+        return screen;
+    }
+
+    private static string DurationString(CodingSession session)
+    {
+        if (session.Duration.TotalHours >= 1)
+        {
+            return $"{(int)session.Duration.TotalHours}h {session.Duration.Minutes}m {(int)Math.Round(session.Duration.TotalSeconds % 60)}s";
+        }
+        else if (session.Duration.TotalMinutes >= 1)
+        {
+            return $"{session.Duration.Minutes}m {(int)Math.Round(session.Duration.TotalSeconds % 60)}s";
+        }
+        else
+        {
+            return $"{(int)Math.Round(session.Duration.TotalSeconds)}s";
+        }
+    }
+}
