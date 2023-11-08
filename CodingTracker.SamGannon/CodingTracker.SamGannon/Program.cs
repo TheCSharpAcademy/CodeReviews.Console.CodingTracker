e225a9b38e47d5d694e99d5c89523d410871fb10
Samuel Gannon <sam.gannon@sportssouth.biz>
2023-11-08 16:40:44 -0600

    intial commit

---
+++ CodingTracker.SamGannon/CodingTracker.SamGannon/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CodingTracker.SamGannon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
