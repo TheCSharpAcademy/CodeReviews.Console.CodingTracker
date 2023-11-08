bf8d8ed32a4bb231984ec03bfacc645ae9e6df5e
Samuel Gannon <sam.gannon@sportssouth.biz>
2023-11-08 17:15:34 -0600

    added and configured dbManager class

---
+++ CodingTracker.SamGannon/CodingTracker.SamGannon/DatabaseManager.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace CodingTracker.SamGannon
+{
+    internal class DatabaseManager
+    {
+        public DatabaseManager()
+        {
+
+        }
+        public void CreateTable(string connectionString)
+        {
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+
+                var tableCmd = connection.CreateCommand();
+
+                tableCmd.CommandText =
+                    @"CREATE TABLE IF NOT EXISTS coding (
+                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        Date TEXT,
+                        Duration TEXT
+                    )";
+                tableCmd.ExecuteNonQuery();
+
+                connection.Close();
+            }
+        }
+
+    }
+}
--- CodingTracker.SamGannon/CodingTracker.SamGannon/Program.cs
+++ CodingTracker.SamGannon/CodingTracker.SamGannon/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace CodingTracker.SamGannon
+﻿using System.Data.Common;
+using System.Configuration;
+
+namespace CodingTracker.SamGannon
 {
     internal class Program
     {
+        
+        static string connectionString = ConfigurationManager.ConnectionStrings["DataConnection"].ConnectionString;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DatabaseManager databaseManager = new();
+            databaseManager.CreateTable(connectionString);
         }
     }
 }
