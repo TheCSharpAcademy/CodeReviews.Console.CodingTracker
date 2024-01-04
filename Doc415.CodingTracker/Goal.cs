3a416f52a552d3b80ab82301ed6093643e22985d
doc415 <sipahioglu_serdar@yahoo.com>
2024-01-04 21:51:56 +0300

    Completed with all challenges

---
+++ Doc415.CodingTracker/Goal.cs
@@ -0,0 +1,9 @@
+﻿namespace Doc415.CodingTracker;
+
+public class Goal
+{
+    public string startDate;
+    public string endDate;
+    public string codingGoal;
+    public long id;
+}
---
+++ Doc415.CodingTracker/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Doc415.CodingTracker;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var DataAccess = new DataAccess();
+
+        DataAccess.CreateDatabase();
+        SeedData.SeedRecords(20);
+        UserInterface.MainMenu();
+    }
+}
