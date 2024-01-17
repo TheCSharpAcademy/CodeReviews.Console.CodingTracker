6f51de72f5f69b7fd0b808024084c2112dbe0e94
frockett <crockett.d.ford@gmail.com>
2024-01-17 10:52:18 +0800

    Initial commit

---
+++ frockett.CodingTracker/frockett.CodingTracker/Program.cs
@@ -0,0 +1,11 @@
+﻿using Spectre.Console;
+
+namespace frockett.CodingTracker;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        AnsiConsole.Markup("[underline red]Hello[/] World!");
+    }
+}
