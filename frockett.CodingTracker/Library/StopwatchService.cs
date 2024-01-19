ceed9087630d7f20ce3c7afa23678627808e3933
frockett <crockett.d.ford@gmail.com>
2024-01-19 23:27:56 +0800

    codacy suggestions done

--- frockett.CodingTracker/Library/StopwatchService.cs
+++ frockett.CodingTracker/Library/StopwatchService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 
 namespace Library
 {
@@ -36,8 +31,5 @@ namespace Library
         {
             return stopwatch.Elapsed;
         }
-
-
-
     }
 }
