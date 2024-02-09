4e8338f3c10193d2377b2a72c04bea05627f4a9c
Pugnetta <115831639+Pugnetta@users.noreply.github.com>
2024-02-09 01:49:49 +0100

    removed usings

--- Buutyful.Coding Tracker/Buutyful.Coding Tracker/Constants.cs
+++ Buutyful.Coding Tracker/Buutyful.Coding Tracker/Constants.cs
@@ -1,9 +1,7 @@
-﻿using System.Configuration;
-
-namespace Buutyful.Coding_Tracker;
+﻿namespace Buutyful.Coding_Tracker;
 
 public static class Constants
 {
-    public static string ConnectionString => ConfigurationManager.AppSettings.Get("ConnectionString") ?? "";
+    public static string ConnectionString => System.Configuration.ConfigurationManager.AppSettings.Get("ConnectionString") ?? "";
     public static string DateFormat => "yyyy-MM-dd HH:mm:ss";
 }
