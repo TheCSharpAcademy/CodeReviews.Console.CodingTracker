580290c7d878a5d7b06ad8229a71cdee7eaf8928
Mateusz-Platek <mplatek1705@gmail.com>
2024-02-17 11:09:08 +0100

    Added connection per request

--- CodingTracker.Mateusz-Platek/CodingTracker.Mateusz-Platek/Database.cs
+++ CodingTracker.Mateusz-Platek/CodingTracker.Mateusz-Platek/Database.cs
@@ -5,69 +5,79 @@ namespace CodingTracker.Mateusz_Platek;
 
 public static class Database
 {
-    private static SqliteConnection sqliteConnection = new SqliteConnection(ConfigurationManager.AppSettings.Get("connectionString"));
-
     public static List<Session> GetSessions()
     {
         List<Session> sessions = new List<Session>();
-        
-        sqliteConnection.Open();
-        SqliteCommand sqliteCommand = sqliteConnection.CreateCommand();
-        sqliteCommand.CommandText = "SELECT * FROM sessions";
-        SqliteDataReader sqliteDataReader = sqliteCommand.ExecuteReader();
-        
-        if (sqliteDataReader.HasRows)
+
+        using (SqliteConnection sqliteConnection = new SqliteConnection(ConfigurationManager.AppSettings.Get("connectionString")))
         {
-            while (sqliteDataReader.Read())
+            sqliteConnection.Open();
+            SqliteCommand sqliteCommand = sqliteConnection.CreateCommand();
+            sqliteCommand.CommandText = "SELECT * FROM sessions";
+            SqliteDataReader sqliteDataReader = sqliteCommand.ExecuteReader();
+        
+            if (sqliteDataReader.HasRows)
             {
-                string start = sqliteDataReader.GetString(2);
-                DateTime dateTimeStart = DateTime.Parse(start);
-                string end = sqliteDataReader.GetString(3);
-                DateTime dateTimeEnd = DateTime.Parse(end);
-                sessions.Add(new Session(
-                    sqliteDataReader.GetInt32(0),
-                    sqliteDataReader.GetString(1),
-                    dateTimeStart,
-                    dateTimeEnd
-                ));
+                while (sqliteDataReader.Read())
+                {
+                    string start = sqliteDataReader.GetString(2);
+                    DateTime dateTimeStart = DateTime.Parse(start);
+                    string end = sqliteDataReader.GetString(3);
+                    DateTime dateTimeEnd = DateTime.Parse(end);
+                    sessions.Add(new Session(
+                        sqliteDataReader.GetInt32(0),
+                        sqliteDataReader.GetString(1),
+                        dateTimeStart,
+                        dateTimeEnd
+                    ));
+                }
             }
         }
-        sqliteConnection.Close();
         
         return sessions;
     }
 
     public static void AddSession(Session session)
     {
-        sqliteConnection.Open();
-        SqliteCommand sqliteCommand = sqliteConnection.CreateCommand();
-        string name = session.name;
-        string start = session.GetStart();
-        string end = session.GetEnd();
-        sqliteCommand.CommandText = $"INSERT INTO sessions (name, start, end) VALUES ('{name}', '{start}', '{end}')";
-        sqliteCommand.ExecuteNonQuery();
-        sqliteConnection.Close();
+        using (SqliteConnection sqliteConnection = new SqliteConnection(ConfigurationManager.AppSettings.Get("connectionString")))
+        {
+            sqliteConnection.Open();
+            SqliteCommand sqliteCommand = sqliteConnection.CreateCommand();
+        
+            string name = session.name;
+            string start = session.GetStart();
+            string end = session.GetEnd();
+        
+            sqliteCommand.CommandText = $"INSERT INTO sessions (name, start, end) VALUES ('{name}', '{start}', '{end}')";
+            sqliteCommand.ExecuteNonQuery();
+        }
     }
 
     public static void UpdateSession(Session session)
     {
-        sqliteConnection.Open();
-        SqliteCommand sqliteCommand = sqliteConnection.CreateCommand();
-        int id = session.id;
-        string name = session.name;
-        string start = session.GetStart();
-        string end = session.GetEnd();
-        sqliteCommand.CommandText = $"UPDATE sessions SET name = '{name}', start = '{start}', end = '{end}' WHERE sessionID = {id}";
-        sqliteCommand.ExecuteNonQuery();
-        sqliteConnection.Close();
+        using (SqliteConnection sqliteConnection = new SqliteConnection(ConfigurationManager.AppSettings.Get("connectionString")))
+        {
+            sqliteConnection.Open();
+            SqliteCommand sqliteCommand = sqliteConnection.CreateCommand();
+        
+            int id = session.id;
+            string name = session.name;
+            string start = session.GetStart();
+            string end = session.GetEnd();
+        
+            sqliteCommand.CommandText = $"UPDATE sessions SET name = '{name}', start = '{start}', end = '{end}' WHERE sessionID = {id}";
+            sqliteCommand.ExecuteNonQuery();
+        }
     }
 
     public static void DeleteSession(int id)
     {
-        sqliteConnection.Open();
-        SqliteCommand sqliteCommand = sqliteConnection.CreateCommand();
-        sqliteCommand.CommandText = $"DELETE FROM sessions WHERE sessionID = {id}";
-        sqliteCommand.ExecuteNonQuery();
-        sqliteConnection.Close();
+        using (SqliteConnection sqliteConnection = new SqliteConnection(ConfigurationManager.AppSettings.Get("connectionString")))
+        {
+            sqliteConnection.Open();
+            SqliteCommand sqliteCommand = sqliteConnection.CreateCommand();
+            sqliteCommand.CommandText = $"DELETE FROM sessions WHERE sessionID = {id}";
+            sqliteCommand.ExecuteNonQuery();
+        }
     }
 }
