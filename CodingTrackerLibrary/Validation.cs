e503fb3b1f66c37c6fd186d40f195d4145a7555f
Joshua Abbey <Joshuaabbey2022@gmail.com>
2024-02-25 17:02:32 +0000

    removed if statement

--- CodingTrackerLibrary/Validation.cs
+++ CodingTrackerLibrary/Validation.cs
@@ -7,12 +7,8 @@
         {
             bool startIsValid = DateTime.TryParseExact(dateStart,"dd/MM/yyyy HH:mm",System.Globalization.CultureInfo.InvariantCulture,System.Globalization.DateTimeStyles.None, out DateTime startResult);
             bool endIsValid = DateTime.TryParseExact(dateEnd, "dd/MM/yyyy HH:mm", System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out DateTime endResult);
+            return startIsValid & endIsValid;
 
-            if (startIsValid & endIsValid)
-                return true;
-
-            else
-                return false;
         }
     }
 }
