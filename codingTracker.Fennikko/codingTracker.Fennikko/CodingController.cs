fae1d416b6e9518e460c71a52459b04d7cf986c7
Keira Krieger <keira.f.krieger@gmail.com>
2024-03-01 19:14:05 -0800

    Created initial classes. Created CodingSession data and duration method. Created CodingController methods.

---
+++ codingTracker.Fennikko/codingTracker.Fennikko/CodingController.cs
@@ -0,0 +1,40 @@
+﻿using System.Configuration;
+using System.Data.Common;
+using codingTracker.Fennikko.Models;
+using Dapper;
+using Microsoft.Data.Sqlite;
+
+public class CodingController
+{
+    private static readonly string ConnectionString = ConfigurationManager.AppSettings.Get("connectionString") +
+                                                      ConfigurationManager.AppSettings.Get("databasePath");
+
+    public static void DatabaseCreation()
+    {
+        using var connection = new SqliteConnection(ConnectionString);
+        connection.Execute(
+            """
+            CREATE TABLE IF NOT EXISTS coding_tracker (
+                 Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                 StartTime TEXT,
+                 EndTime TEXT,
+                 Duration TEXT
+                 )               
+            """);
+    }
+
+    public static void Session()
+    {
+
+    }
+
+    public static void GetAllSessions()
+    {
+
+    }
+
+    public static void TableCreation(IEnumerable<CodingSession> sessions)
+    {
+
+    }
+}
---
+++ codingTracker.Fennikko/codingTracker.Fennikko/Models/CodingSession.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+
+namespace codingTracker.Fennikko.Models
+{
+    public class CodingSession
+    {
+        public int ID { get; set; }
+
+        public string? StartTime { get; set; }
+
+        public string? EndTime { get; set;}
+
+        public string Duration {get{return GetDuration(StartTime,EndTime);}}
+
+
+
+        public static string GetDuration(string startTime, string endTime)
+        {
+            var startDateTime = DateTime.ParseExact(startTime, "dd-MM-yy HH:mm", new CultureInfo("en-US"));
+            var endDateTime = DateTime.ParseExact(endTime, "dd-MM-yy HH:mm", new CultureInfo("en-US"));
+
+            var differenceOfDates = endDateTime.Subtract(startDateTime);
+            string stringDifference;
+
+            switch (differenceOfDates.TotalHours)
+            {
+                case < 1:
+                    stringDifference = Convert.ToString($"{differenceOfDates.TotalMinutes:##.00} minutes");
+                    break;
+                case >= 1 and <= 24:
+                    stringDifference = Convert.ToString($"{differenceOfDates.TotalHours:##.00} hours");
+                    break;
+                default:
+                    stringDifference = Convert.ToString($"{differenceOfDates.TotalDays:##.00} days");
+                    break;
+            }
+            return stringDifference;
+        }
+    }
+}
