4875d6f407f40fb643095bf9e7b46f3b681fb96f
Emgigas <119613722+Emgigas@users.noreply.github.com>
2024-04-08 21:22:43 +0100

    Creation of config file and testing it works

---
+++ CodingTracker/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Configuration;
+using System.Collections.Specialized;
+
+string sAttr;
+
+sAttr = ConfigurationManager.AppSettings.Get("connectionString");
+
+Console.WriteLine(sAttr);
