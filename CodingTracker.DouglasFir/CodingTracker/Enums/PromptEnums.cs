71ca620408fb522fd54295d2d74d2f85d2e51cf7
Douglas Rank <douglas.rankjr@gmail.com>
2024-04-19 12:23:51 -0700

    fixed code checks

--- CodingTracker.DouglasFir/CodingTracker/Enums/PromptEnums.cs
+++
@@ -1,6 +0,0 @@
-﻿public enum DatePrompt
-{
-    Short,
-    Long,
-}
-
--- CodingTracker.DouglasFir/CodingTracker/Models/SessionStatistics.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CodingTracker.Models;
-
-public class SessionStatistics
-{
-    public int TotalSessions { get; set; }
-    public double AverageDuration { get; set; }
-}
