680bd595d9d102dd7058087d9810319863bd8c4b
N-Endy <okafornel@gmail.com>
2024-05-23 13:00:54 +0100

    Update InsertSession method in Service

--- Nelson.CodingTracker/CodingTracker/Services/CodingSessionService.cs
+++ Nelson.CodingTracker/CodingTracker/Services/CodingSessionService.cs
@@ -1,15 +1,18 @@
 using CodingTracker.Models;
 using CodingTracker.IDataRepository;
+using CodingTracker.Utilities;
 
 namespace CodingTracker.Services
 {
     public class CodingSessionService : ICodingSessionService
     {
         private readonly ICodingSessionRepository _sessionRepository;
+        private readonly IUtils _utils;
 
-        public CodingSessionService(ICodingSessionRepository sessionRepository)
+        public CodingSessionService(ICodingSessionRepository sessionRepository, IUtils utils)
         {
             _sessionRepository = sessionRepository;
+            _utils = utils;
         }
 
         public void DeleteSession()
@@ -24,7 +27,12 @@ namespace CodingTracker.Services
 
         public void InsertSession(CodingSession session)
         {
-            throw new NotImplementedException();
+            var listOfTimes = _utils.ValidatedTimes();
+            session.StartTime = listOfTimes[0];
+            session.EndTime = listOfTimes[1];
+            session.Duration = _utils.GetSessionDuration(session.StartTime, session.EndTime);
+
+            _sessionRepository.InsertSessionToDatabase(session.StartTime, session.EndTime, session.Duration);
         }
 
         public void UpdateSession()
