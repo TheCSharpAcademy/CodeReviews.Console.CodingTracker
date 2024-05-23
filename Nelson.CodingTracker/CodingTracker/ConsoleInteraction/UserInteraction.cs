bd95f974d2f382e3ccf0317dfd32dcdfb15e4bb8
N-Endy <okafornel@gmail.com>
2024-05-23 10:15:18 +0100

    Update Display Menu method with Spectre

--- Nelson.CodingTracker/CodingTracker/ConsoleInteraction/UserInteraction.cs
+++ Nelson.CodingTracker/CodingTracker/ConsoleInteraction/UserInteraction.cs
@@ -1,19 +1,35 @@
+using Spectre.Console;
+
 namespace CodingTracker.ConsoleInteraction
 {
     public class UserInteraction : IUserInteraction
     {
+        // public void DisplayMenu()
+        // {
+        //     Console.WriteLine("\n\nMain Menu");
+        //     Console.WriteLine("\nWhat would you like to do?");
+        //     Console.WriteLine("\nType 0 to Close Application");
+        //     Console.WriteLine("Type 1 to View All Coding Sessions");
+        //     Console.WriteLine("Type 2 to Insert New Coding Session");
+        //     Console.WriteLine("Type 3 to Update Coding Session");
+        //     Console.WriteLine("Type 4 to Delete Coding Session");
+        //     Console.WriteLine("----------------------------------------\n");
+        //     Console.Write("Input: ");
+        // }
+
         public void DisplayMenu()
         {
-            Console.WriteLine("\n\nMain Menu");
-            Console.WriteLine("\nWhat would you like to do?");
-            Console.WriteLine("\nType 0 to Close Application");
-            Console.WriteLine("Type 1 to View All Coding Sessions");
-            Console.WriteLine("Type 2 to Insert New Coding Session");
-            Console.WriteLine("Type 3 to Update Coding Session");
-            Console.WriteLine("Type 4 to Delete Coding Session");
-            Console.WriteLine("----------------------------------------\n");
-            Console.Write("Input: ");
+            AnsiConsole.Write(new Markup("\n\nMain Menu"));
+            AnsiConsole.Write(new Markup("\nWhat would you like to do?"));
+            AnsiConsole.Write(new Markup("\n\n[DarkRed]Type 0 to Close Application[/]"));
+            AnsiConsole.Write(new Markup("\n[Green]Type 1 to View All Coding Sessions[/]"));
+            AnsiConsole.Write(new Markup("\n[Yellow]Type 2 to Insert New Coding Session[/]"));
+            AnsiConsole.Write(new Markup("\n[DarkGreen]Type 3 to Update Coding Session[/]"));
+            AnsiConsole.Write(new Markup("\n[Red]Type 4 to Delete Coding Session[/]"));
+            AnsiConsole.Write(new Markup("\n----------------------------------------\n"));
+            AnsiConsole.Write(new Markup("Input: "));
         }
+
 
         public string GetUserInput()
         {
