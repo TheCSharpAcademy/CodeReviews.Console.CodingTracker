3f04e22b32f067219b18ffdae9cb811ec5bf8111
N-Endy <okafornel@gmail.com>
2024-05-23 13:02:26 +0100

    Update code for app startup

--- Nelson.CodingTracker/CodingTracker/Program.cs
+++ Nelson.CodingTracker/CodingTracker/Program.cs
@@ -3,11 +3,13 @@ using CodingTracker.Controllers;
 using CodingTracker.DataRepository;
 using CodingTracker.IDataRepository;
 using CodingTracker.Services;
+using CodingTracker.Utilities;
 
 DataConfig dataConfig = new();
 IUserInteraction userInteraction= new UserInteraction();
 ICodingSessionRepository codingSessionRepository= new CodingSessionRepository(userInteraction);
-ICodingSessionService codingSessionService= new CodingSessionService(codingSessionRepository);
+IUtils utils = new Utils(userInteraction);
+ICodingSessionService codingSessionService= new CodingSessionService(codingSessionRepository, utils);
 CodingSessionController codingSessionController= new(userInteraction, codingSessionService, dataConfig);
 
 codingSessionController.Run();
