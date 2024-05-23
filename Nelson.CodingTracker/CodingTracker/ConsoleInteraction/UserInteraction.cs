15abedea453802ed21ecb28bf5c2ce57bad64a07
N-Endy <okafornel@gmail.com>
2024-05-23 10:23:12 +0100

    Update methods with Spectre

--- Nelson.CodingTracker/CodingTracker/ConsoleInteraction/UserInteraction.cs
+++ Nelson.CodingTracker/CodingTracker/ConsoleInteraction/UserInteraction.cs
@@ -4,19 +4,6 @@ namespace CodingTracker.ConsoleInteraction
 {
     public class UserInteraction : IUserInteraction
     {
-        // public void DisplayMenu()
-        // {
-        //     Console.WriteLine("\n\nMain Menu");
-        //     Console.WriteLine("\nWhat would you like to do?");
-        //     Console.WriteLine("\nType 0 to Close Application");
-        //     Console.WriteLine("Type 1 to View All Coding Sessions");
-        //     Console.WriteLine("Type 2 to Insert New Coding Session");
-        //     Console.WriteLine("Type 3 to Update Coding Session");
-        //     Console.WriteLine("Type 4 to Delete Coding Session");
-        //     Console.WriteLine("----------------------------------------\n");
-        //     Console.Write("Input: ");
-        // }
-
         public void DisplayMenu()
         {
             AnsiConsole.Write(new Markup("\n\nMain Menu"));
@@ -38,13 +25,13 @@ namespace CodingTracker.ConsoleInteraction
 
         public void ShowMessage(string message)
         {
-            Console.WriteLine(message);
+            AnsiConsole.Write(new Markup(message));
         }
 
         public void ShowMessageTimeout(string message)
         {
             Thread.Sleep(500);
-            Console.WriteLine(message);
+            AnsiConsole.Write(new Markup(message));
             Thread.Sleep(500);
         }
     }
