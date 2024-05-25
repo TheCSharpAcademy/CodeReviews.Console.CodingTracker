b9c7b3d68781e3c01d648ad94de920e2779b086d
N-Endy <okafornel@gmail.com>
2024-05-25 21:27:41 +0100

    Change type of Duration to string

--- Nelson.CodingTracker/CodingTracker/Models/CodingSession.cs
+++ Nelson.CodingTracker/CodingTracker/Models/CodingSession.cs
@@ -3,8 +3,8 @@ namespace CodingTracker.Models
     public class CodingSession
     {
         public int Id {get; set; }
-        public DateTime StartTime { get; set; } = DateTime.Now;
-        public DateTime EndTime { get; set; } = DateTime.Now;
-        public TimeSpan Duration { get; set; } = DateTime.Now - DateTime.Now;
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string? Duration { get; set; }
     }
 }
