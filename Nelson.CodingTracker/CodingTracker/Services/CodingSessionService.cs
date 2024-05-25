1ccaf5aabf43370335f65a91e6be684fda253622
N-Endy <okafornel@gmail.com>
2024-05-25 23:18:56 +0100

    Update call to database in UpdateSession method

--- Nelson.CodingTracker/CodingTracker/Services/CodingSessionService.cs
+++ Nelson.CodingTracker/CodingTracker/Services/CodingSessionService.cs
@@ -1,17 +1,20 @@
 using CodingTracker.Models;
 using CodingTracker.IDataRepository;
 using CodingTracker.Utilities;
+using CodingTracker.ConsoleInteraction;
 
 namespace CodingTracker.Services
 {
     public class CodingSessionService : ICodingSessionService
     {
         private readonly ICodingSessionRepository _sessionRepository;
+        private readonly IUserInteraction _userInteraction;
         private readonly IUtils _utils;
 
-        public CodingSessionService(ICodingSessionRepository sessionRepository, IUtils utils)
+        public CodingSessionService(ICodingSessionRepository sessionRepository, IUtils utils, IUserInteraction userInteraction)
         {
             _sessionRepository = sessionRepository;
+            _userInteraction = userInteraction;
             _utils = utils;
         }
 
@@ -37,7 +40,29 @@ namespace CodingTracker.Services
 
         public void UpdateSession()
         {
-            throw new NotImplementedException();
+            int id = _utils.ConvertToInt(_userInteraction.GetUserInput());
+
+            if (id == 0) return;
+
+            _userInteraction.ShowMessage("\n[Yellow]Which property would you like to update[/]");
+            _userInteraction.ShowMessage("\n[DarkGreen]1. Start Time[/]");
+            _userInteraction.ShowMessage("\n[DarkGreen]2. End Time[/]");
+            _userInteraction.ShowMessage("\n[Green]Input: [/]");
+
+            switch (_userInteraction.GetUserInput())
+            {
+                case "1":
+                    DateTime startTime = _utils.ValidatedStartTime();
+                    _sessionRepository.UpdateStartTimeInDatabase(id, startTime);
+                    break;
+                case "2":
+                    DateTime endTime = _utils.ValidatedEndTime();
+                    _sessionRepository.UpdateEndTimeInDatabase(id, endTime);
+                    break;
+                default:
+                    _userInteraction.ShowMessageTimeout("\n[Red]You have inputed a wrong choice. Going back to Menu[/]\n");
+                    break;
+            }
         }
     }
 }
