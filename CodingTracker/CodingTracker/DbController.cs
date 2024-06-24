96ca766cec3058995d150ccfc34d82b961d439ec
kjanos89 <91398032+kjanos89@users.noreply.github.com>
2024-06-24 20:37:02 +0200

    Database cleanup and Insert command
    Renamed 1-2 variables to be more readable, created the db creation method, created the insert records method.

--- CodingTracker/CodingTracker/DbController.cs
+++ CodingTracker/CodingTracker/DbController.cs
@@ -3,21 +3,97 @@ using System.Configuration;
 using Dapper;
 using System;
 using System.Data.SqlClient;
+using Spectre.Console;
 namespace CodingTracker
 {
     public class DbController
     {
         private readonly string _connectionString;
-        public SQLiteConnection SQLiteConnection { get; private set; }
+        public SQLiteConnection _connection { get; private set; }
         public DbController()
         {
             _connectionString = ConfigurationManager.AppSettings["connectionString"];
-            SQLiteConnection = new SQLiteConnection("Data Source=CodingSessions.db;");
+            _connection = new SQLiteConnection("Data Source=CodingSessions.db;");
             if(!File.Exists("./CodingSessions.db"))
             {
-
+                SQLiteConnection.CreateFile("CodingSessions.db");
+                AnsiConsole.MarkupLine("[underline bold]The database has been created![/]");
+            }
+            using (var connection = new SQLiteConnection(_connection))
+            {
+                connection.Open();
+                var tableCommand = _connection.CreateCommand();
+                tableCommand.CommandText = @"
+            CREATE TABLE IF NOT EXISTS Sessions (
+                Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                StartDate TEXT
+                EndDate TEXT,
+                Duration TEXT
+            )";
+                tableCommand.ExecuteNonQuery();
+                _connection.Close();
+                AnsiConsole.MarkupLine("[yellow bold]The table has been created and is ready to use![/]");
             }
             
         }
+        public void ReadRecords()
+        {
+            using (var connection = new SQLiteConnection(_connection))
+            {
+                connection.Open();
+                var selectCommand = connection.CreateCommand();
+                selectCommand.CommandText = "SELECT * FROM coding";
+
+                using (var reader = selectCommand.ExecuteReader())
+                {
+                    bool hasRows = false;
+                    while (reader.Read())
+                    {
+                        hasRows = true;
+                        var id = reader.GetInt32(0);
+                        var startDate = reader.GetString(1);
+                        var endDate = reader.GetString(2);
+                        var duration = reader.GetString(3);
+                        Console.WriteLine($"Id: {id}, Beginning date: {startDate}, Ending date: {endDate} Total duration: {duration}");
+                    }
+                    if (!hasRows)
+                    {
+                        Console.WriteLine("No records found.");
+                    }
+                }
+                connection.Close();
+            }
+        }
+        public void InsertRecords()
+        {
+            AnsiConsole.WriteLine("[green]Please type in the date of the start below:");
+            string startDate = Console.ReadLine();
+            Validation validation = new Validation();
+            if (!String.IsNullOrEmpty(startDate))
+            {
+                validation.ValidString(startDate); //placeholder method, will check the input here.
+            }
+            AnsiConsole.WriteLine("[green]Please type in the date of the end below:");
+            string endDate = Console.ReadLine();
+            if (!String.IsNullOrEmpty(endDate))
+            {
+                validation.ValidString(endDate); //placeholder method, will check the input here.
+            }
+            string duration = Console.ReadLine(); //placeholder for duration counting
+            using (var connection = new SQLiteConnection(_connection))
+            {
+                connection.Open();
+                var insertCommand = connection.CreateCommand();
+                insertCommand.CommandText = @"INSERT INTO Sessions (StartDate, EndDate, Duration)
+                                              VALUES (@StartDate, @EndDate, @Duration)";
+                insertCommand.Parameters.AddWithValue("@StartDate", startDate);
+                insertCommand.Parameters.AddWithValue("@EndDate", endDate);
+                insertCommand.Parameters.AddWithValue("@Duration", duration);
+                insertCommand.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+
+
     }
 }
