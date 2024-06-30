84f566a45118bb2d807d191854136607cb42cc86
tonyissa <52683702+tonyissa@users.noreply.github.com>
2024-06-30 19:50:16 -0400

    Add CodingSession model

---
+++ Console.CodingTracker.tonyissa/Models/CodingSession.cs
@@ -0,0 +1,17 @@
+﻿namespace CodingTracker.CodingSession
+{
+    public class CodingSession
+    {
+        public int ID;
+        public DateTime Start;
+        public DateTime End;
+
+        public CodingSession(int id, DateTime start, DateTime end)
+        {
+            ID = id;
+            Start = start;
+            End = end;
+        }
+    }
+
+}
