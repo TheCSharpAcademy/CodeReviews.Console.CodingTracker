b212521e3d2b03053e9ee24f11fe1f3cdba5acb2
Alvin Graham <grah0071@gmail.com>
2024-07-01 23:13:01 -0500

    Create configs

--- CodingTracker.AlvinGraham/CodingTracker.AlvinGraham/Program.cs
+++ CodingTracker.AlvinGraham/CodingTracker.AlvinGraham/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using Microsoft.Extensions.Configuration;
+
+
+IConfiguration configuration = new ConfigurationBuilder()
+	.AddJsonFile("appsettings.json")
+	.Build();
+
+string connectionString = configuration.GetSection("ConnectionStrings")["DefaultConnection"]!;
+
+Console.WriteLine(connectionString);
