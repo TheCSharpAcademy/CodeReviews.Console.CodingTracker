9e621d67a07c71ebfdc36631888471700bc9f72a
tonyissa <52683702+tonyissa@users.noreply.github.com>
2024-07-05 23:21:10 -0400

    Move datetime input calling function into Input namespace

--- Console.CodingTracker.tonyissa/Input.cs
+++ Console.CodingTracker.tonyissa/Input.cs
@@ -41,5 +41,34 @@ namespace CodingTracker.Input
                 Console.WriteLine("Invalid time.");
             }
         }
+
+        public static (DateTime, DateTime)? GetAllDateTimes()
+        {
+            var startDate = GetDateInput(true);
+            if (startDate == "0") return null;
+            else if (startDate == "") startDate = DateHelper.GetCurrentDate();
+
+            var startTime = GetTimeInput(true);
+            if (startTime == "0") return null;
+            else if (startTime == "") startTime = DateHelper.GetCurrentTime();
+
+            var endDate = GetDateInput(false);
+            if (endDate == "0") return null;
+            else if (endDate == "") endDate = DateHelper.GetCurrentDate();
+
+            var endTime = GetTimeInput(false);
+            if (endTime == "0") return null;
+            else if (endTime == "") endTime = DateHelper.GetCurrentTime();
+
+            var date1 = DateTime.Parse($"{startDate} {startTime}");
+            var date2 = DateTime.Parse($"{endDate} {endTime}");
+
+            if (!DateHelper.CompareDates(date1, date2))
+            {
+                throw new ArgumentException("Date 1 equal or later than date 2.");
+            }
+
+            return (date1, date2);
+        }
     }
 }
--- Console.CodingTracker.tonyissa/UserInterface.cs
+++ Console.CodingTracker.tonyissa/UserInterface.cs
@@ -62,7 +62,7 @@ namespace CodingTracker.UserInterface
         {
             Console.Clear();
 
-            var result = GetAllDateTimes();
+            var result = InputHelper.GetAllDateTimes();
 
             if (result == null) return;
 
@@ -99,35 +99,6 @@ namespace CodingTracker.UserInterface
                     Console.ReadKey();
                 }
             }
-        }
-
-        public static (DateTime, DateTime)? GetAllDateTimes()
-        {
-            var startDate = InputHelper.GetDateInput(true);
-            if (startDate == "0") return null;
-            else if (startDate == "") startDate = DateHelper.GetCurrentDate();
-
-            var startTime = InputHelper.GetTimeInput(true);
-            if (startTime == "0") return null;
-            else if (startTime == "") startTime = DateHelper.GetCurrentTime();
-
-            var endDate = InputHelper.GetDateInput(false);
-            if (endDate == "0") return null;
-            else if (endDate == "") endDate = DateHelper.GetCurrentDate();
-
-            var endTime = InputHelper.GetTimeInput(false);
-            if (endTime == "0") return null;
-            else if (endTime == "") endTime = DateHelper.GetCurrentTime();
-
-            var date1 = DateTime.Parse($"{startDate} {startTime}");
-            var date2 = DateTime.Parse($"{endDate} {endTime}");
-
-            if (!DateHelper.CompareDates(date1, date2))
-            {
-                throw new ArgumentException("Date 1 equal or later than date 2.");
-            }
-
-            return (date1, date2);
         }
     }
 }
