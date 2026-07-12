59fa6b34601e9cb930bd9d296f58eadf8045df67
matejadb <92729211+matejadb@users.noreply.github.com>
2026-07-12 13:34:53 +0200

    implement 2 datetime validation unit tests and 1 duration calculation unit test.

---
+++ CodingTracker.matejadb.UnitTests/DurationCalculationTests.cs
@@ -0,0 +1,17 @@
+﻿using CodingTracker.matejadb.Utils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTracker.matejadb.UnitTests; 
+public class DurationCalculationTests {
+    [Test]
+    public void ValidDateTimeInputs_ReturnsCorrectlyCalculatedDurationInMinutes() {
+        var startTime = "2026-07-12 13:30";
+        var endTime = "2026-07-12 14:00";
+
+        var duration = CalculateSessionDuration.SessionDuration(startTime, endTime);
+
+        Assert.That(duration, Is.EqualTo("30 minutes"));
+    }
+}
---
+++ CodingTracker.matejadb.UnitTests/ValidationTests.cs
@@ -0,0 +1,22 @@
+﻿using CodingTracker.matejadb.Utils;
+
+namespace CodingTracker.matejadb.UnitTests {
+    public class ValidationTests {
+
+        [Test]
+        public void CorrectDateTimeInput_ReturnsTrue() {
+
+            var isValidDateTime = Validation.ValidateDateTime("2026-07-12 13:30");
+
+            Assert.That(isValidDateTime, Is.True);
+        }
+
+        [Test]
+        public void InvalidDateTimeInput_ReturnsFalse() {
+            var isValidDateTime = Validation.ValidateDateTime("2026-7-12 1:30");
+
+            Assert.That(isValidDateTime, Is.False);
+
+        }
+    }
+}
