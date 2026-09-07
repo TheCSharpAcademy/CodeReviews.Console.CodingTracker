af409f39d972dfbd173bf107dadd6008820c6493
Kurtić Džemal <Dzemaludeen@gmail.com>
2026-09-07 09:37:06 +0200

    Add CodingSession model

---
+++ CodingTracker.DzemalKurtic/CodingTracker.DzemalKurtic/Models/CodingSession.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTracker.DzemalKurtic.Models
+{
+    internal class CodingSession
+    {
+        public int Id { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        private TimeSpan Duration => EndTime - StartTime;
+        public CodingSession(int id, DateTime startTime, DateTime endTime)
+        {
+            Id = id;
+            StartTime = startTime;
+            EndTime = endTime;
+        }
+    }
+}
---
+++ CodingTracker.DzemalKurtic/CodingTracker.DzemalKurtic/Program.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.Extensions.Configuration;
+
+IConfiguration config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+var connectionString = config.GetConnectionString("conectionString");
